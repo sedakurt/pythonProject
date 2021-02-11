119a14162cacd44c7b4e9dd041342d3b94b49b88
seda <sedabyrktr@gmail.com>
2021-02-11 16:02:38 +0300

    commit

--- venv/Polymorphism.py
+++ venv/Polymorphism.py
@@ -1,3 +1,4 @@
+# Parent class 1
 class Person:
     def __init__(self, firstname, lastname, health, status):
 
@@ -35,11 +36,22 @@ Lee = Person("Lee", "Williams", 40, status=True)
 #Seda.introduce()
 #Seda.status_change()
 
-
+#inheritance example
 class Enemy(Person):
     def __init__(self, weapon, firstname,lastname, health, status):
         super().__init__(firstname, lastname, health, status)
         self.weapon = weapon
+
+        """
+        Polymorphism occurs when we want to allow the child class to have a method with the same name and a similar implementation as the parent class and we wish for that method you override the parent class method.
+        """
+
+    def introduce(self):
+        print("You are my mortal enemy!!!")
+
+        """
+        When we make a method in the child's class with the same name as the one in the parent class, the child class method overrides the parent class method.
+        """
 
     def hurt(self, other):
         if self.weapon == 'rock':
@@ -58,6 +70,8 @@ class Enemy(Person):
 
 
 Alex = Enemy('rock', 'Alex', 'Wayne', 75, status=False)
+Alex.introduce()
+
 
 Alex.hurt(Seda)
 Alex.insult(Maria)
